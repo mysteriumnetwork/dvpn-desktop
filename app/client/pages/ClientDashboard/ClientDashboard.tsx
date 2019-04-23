1b0d2da6012f8088992f014a3e7f6faf77ede82d
marianasvi <mariana.svirzhevska@nordwhale.com>
2019-04-23 17:03:50 +0300

    Client Page markup, BottomBar.

--- app/client/pages/ClientDashboard/ClientDashboard.tsx
+++ app/client/pages/ClientDashboard/ClientDashboard.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 
 import SideBar from './components/SideBar/SideBar'
+import BottomBar from './components/BottomBar/BottomBar'
 import ConnectionTable from './components/ConnectionTable/ConnectionTable'
 
 const styles = require('./ClientDashboard.scss')
@@ -9,6 +10,7 @@ const ClientDashboard = () => (
   <div className={styles.dashboardCover}>
     <SideBar />
     <ConnectionTable />
+    <BottomBar />
   </div>
 )
 
