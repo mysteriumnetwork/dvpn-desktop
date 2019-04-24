7250e0c78242aaf61b0550c1ab3d21d2265463d6
marianasvi <mariana.svirzhevska@nordwhale.com>
2019-04-24 09:53:00 +0300

    Client Connected page

--- app/client/pages/ClientConnected/ClientConnected.tsx
+++ app/client/pages/ClientConnected/ClientConnected.tsx
@@ -3,24 +3,33 @@ import { Link } from 'react-router-dom'
 import { NAV_CLIENT_DASHBOARD } from '../../client.links'
 import trans from '../../../trans'
 import Button from '../../../ui-kit/components/Button/Button'
+import ConnectedImgBlock from './components/ConnectedImgBlock/ConnectedImgBlock'
+import ConnectedInfoBlock from './components/ConnectedInfoBlock/ConnectedInfoBlock'
+import BottomBar from './components/BottomBar/BottomBar'
+
+const styles = require('./ClientConnected.scss')
 
 const ClientConnected = () => (
-  <div>
+  <div className={styles.root}>
+    <h3 className={styles.title}>{trans('app.client.connecting.title')}</h3>
     <div>
-      <h4>
-        <p>9{trans('app.node.running.users.connected')}</p>
-        <p>
-          9{trans('app.node.running.successful.connections')}
-          <span>/</span>
-          11{trans('app.node.running.attempted')}
-        </p>
-      </h4>
+      <ConnectedImgBlock />
+      <ConnectedInfoBlock />
+    </div>
+    <div className={styles.action}>
       <Link to={NAV_CLIENT_DASHBOARD}>
-        <Button variant="contained" color="secondary">
-          Disconnect
+        <Button color="primary">
+          <div className={styles.buttonLabel}>
+            <div />
+            <p>{trans('app.client.remove.from.favorites.button')}</p>
+          </div>
         </Button>
       </Link>
+      <Link to={NAV_CLIENT_DASHBOARD}>
+        <Button color="secondary">{trans('app.client.disconnect.button')}</Button>
+      </Link>
     </div>
+    <BottomBar />
   </div>
 )
 
